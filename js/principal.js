12c63f4f829a7dc1acb9f1c77ca48af881a7bc57
MiguelAugusto02 <miguel.coelho.andrade@escola.pr.gov.br>
2025-11-05 10:59:45 -0300

    paciente js

---
+++ js/principal.js
@@ -0,0 +1,9 @@
+
+var titulo = document.querySelector(".titulo");
+
+//console.log("titulo");
+//console.log("titulo.textContent");
+
+titulo.textContent = "Aparecida nutricionista"
+
+/* IMC = massa/altura^2  */
